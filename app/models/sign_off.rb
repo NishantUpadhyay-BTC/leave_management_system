1e570869dd70e75f9cae5755ab4d9a81d691b6dc
RaviMariya-BTC <ravi.mariya@botreetechnologies.com>
2016-11-14 13:08:53 +0530

    fixed design bugs

--- app/models/sign_off.rb
+++ app/models/sign_off.rb
@@ -3,7 +3,7 @@ class SignOff < ActiveRecord::Base
   has_many :users, :through => :sign_off_requesters
   belongs_to :sign_off_type
   has_many :comments
-
+  belongs_to :user
   validates :user_id, :sign_off_type_id, :date_from, :date_to, :half_full_leave, presence: true
 
   enum sign_off_status: { pending: 0, approved: 1 }
